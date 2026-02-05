0523a9392e5a9ff17ae45ceb8e830e1e886ee2d6
anderson.mordhorst <anderson.mordhorst@gmail.com>
2026-02-05 09:05:57 -0300

    feat: armazenando artefatos e modelos no dagshub

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,13 @@
 import logging
 from dotenv import load_dotenv
+import dagshub
 
 load_dotenv()
+
+dagshub.init(
+    repo_owner="anderson-mordhorst",
+    repo_name="curso-mlops"
+)
 
 # Configure the logging strategy
 logging.basicConfig(
